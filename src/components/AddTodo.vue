5add3e77b7906e76c019f4118e3d1ee40c3c1df8
SoemKyaw <soem22645@gmail.com>
2024-01-07 19:40:55 +0630

    change Buttom text to Add Todo text

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -4,7 +4,7 @@
   <b-input-group class="mt-3">
     <b-form-input v-model="newTodo.title"></b-form-input>
     <b-input-group-append>
-      <b-button variant="primary" @click="addTodo(newTodo)">Button</b-button>
+      <b-button variant="primary" @click="addTodo(newTodo)">Add Todo</b-button>
     </b-input-group-append>
   </b-input-group>
 </div>
